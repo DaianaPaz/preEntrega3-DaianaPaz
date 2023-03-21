f95742d184651c8a1aa0cf9db313b7c82e45527f
Daiana Paz <daianadenispaz@gmail.com>
2023-03-20 21:55:20 -0300

    modificacion del codigo

--- main.js
+++ main.js
@@ -1,114 +1,66 @@
 /*ENTRADAS AL CINE*/
 
-function saludar(){
-    alert("Bienvenidos/as");
-}
-
-class Persona{
-    constructor(info){
-        this.nombre = info.tuNombre;
-        this.edad = info.tuEdad;
-        this.entrada = info.tuEntrada;
-        this.formaDePago = info.tuFormaDePago;
-    }
-}
-
-saludar();
-
-let tuNombre;
-let tuEdad;
-let tuEntrada;
-let tuFormaDePago;
-const arrayPersonas = []
-
-for(let i = 0; i < 2; i++){
-
-    tuNombre= prompt("Ingresar nombre");
-    tuNombre = tuNombre.toUpperCase();
-    tuEdad= parseInt(prompt("Ingresar edad"));
-    if(tuEdad > 17){
-                alert("Puede entrar");
-    }else{
-                alert("Debe entrar con un adulto");
-    }
-    tuEntrada= prompt("Tipo de entrada: 2D, 3D o 4D");
-    tuEntrada = tuEntrada.toUpperCase();
-    while(tuEntrada != "2D" && tuEntrada != "3D" && tuEntrada != "4D"){
-        tuEntrada= prompt("Tipo de entrada: 2D, 3D o 4D");
-        tuEntrada = tuEntrada.toUpperCase();
-    }
-    tuFormaDePago= prompt("Forma de pago: EFECTIVO, CREDITO o DEBITO");
-    tuFormaDePago = tuFormaDePago.toUpperCase();
-    while(tuFormaDePago != "EFECTIVO" && tuFormaDePago != "CREDITO" && tuFormaDePago != "DEBITO"){
-        tuFormaDePago= prompt("Forma de pago: EFECTIVO, CREDITO o DEBITO");
-        tuFormaDePago = tuFormaDePago.toUpperCase();
-    }
-    const persona = new Persona(
-        {tuNombre,
-        tuEdad,
-        tuEntrada,
-        tuFormaDePago,}
-    );
-
-    arrayPersonas.push(persona);
-}
-
-console.log(arrayPersonas);
-//MOSTRAR EL NOMBRE Y LA FORMA DE PAGO DE CADA USUARIO
-for(const item of arrayPersonas){
-    console.log(item.nombre);
-    console.log(item.formaDePago);
-}
-
-//OPERACIONES A UTILIZAR
-function operaciones(operacion){
-if(operacion === "sumar"){
-    return (x, y) => x + y;
-}
-
-if(operacion === "restar"){
-    return (x, y) => x - y;
-}
-
-if(operacion === "multiplicar"){
-    return (x, y) => x * y;
-}
-
-if(operacion === "dividir"){
-    return (x, y) => x / y;
-}
-}
-
-//SUMAR LAS EDADES INGRESADAS
-let sumaEdades = 0;
-function iterador(arrayPersonas, funcion){
-    for(const persona of arrayPersonas){
-        sumaEdades += persona.edad;
-    }
-}
-
-iterador(arrayPersonas, operaciones("sumar"));
-console.log(sumaEdades);
-
-//  MOSTRAR USUARIOS MAYORES DE 18 AÑOS
-let usuario = arrayPersonas.filter(item => item.edad > 18);
-console.log(usuario);
-
 // UTILIZACION DEL DOCUMENT
 
-let parrafo = document.createElement("p");
-parrafo.innerHTML = "Son los usuarios ingresados";
-document.body.append(parrafo);
-
-let usuarios = document.getElementById("usuarios");
-arrayPersonas.forEach(item => {
-    let persona = document.createElement("li");
-    persona.innerHTML = `
-    <h2>Nombre: ${item.nombre}</h2>
-    <p>Edad: ${item.edad}</p>
-    <p>Tipo de entrada: ${item.entrada}</p>
-    <p>Forma de pago: ${item.formaDePago}</p>`;
-    usuarios.append(persona);   
+class Persona{
+    constructor(nombre, edad, tipoDeEntrada, formaDePago){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tipoDeEntrada = tipoDeEntrada;
+        this.formaDePago = formaDePago;
+    }
 }
-)
+
+const personas = [];
+
+
+if(localStorage.getItem('personas')){
+    let persona = JSON.parse(localStorage.getItem('personas'));
+    for(let i = 0; i < persona.length; i++){
+        personas.push(persona[i]);
+    }
+}
+
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    agregarPersona();
+});
+
+function agregarPersona(){
+    const nombre = document.getElementById('nombre').value;
+    const edad = document.getElementById('edad').value;
+    const entrada = document.getElementById('entrada').value;
+    const pago = document.getElementById('pago').value;
+    const nuevaPersona = new Persona(nombre, edad, entrada, pago);
+    personas.push(nuevaPersona);
+    localStorage.setItem('personas', JSON.stringify(personas));
+    formulario.reset();
+    
+}
+
+const usuarios = document.getElementById('usuarios');
+
+const verCarrito = document.getElementById('verCarrito');
+
+verCarrito.addEventListener('click', () => {
+    mostrarPersonas();
+});
+
+function mostrarPersonas(){
+    usuarios.innerHTML = '';
+    personas.forEach((persona) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div>
+            <h2>Nombre: ${persona.nombre}</h2>
+            <p>Edad: ${persona.edad}</p>
+            <p>Tipo de entrada: ${persona.tipoDeEntrada}</p>
+            <p>Forma de pago: ${persona.formaDePago}</p>
+        </div>`;
+        usuarios.appendChild(div);
+    });
+}
+
 
